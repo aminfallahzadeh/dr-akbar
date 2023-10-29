235b3f9a4025c7a99ede4f485456d84716ffafea
amin-dev-tech <amin.dev.tech@gmail.com>
2023-10-29 18:03:17 +0300

    change color pallete

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ const navLinks = document.getElementById("links");
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navBar.classList.add("active");
-    navBar.style.padding = "1.5rem 15rem 0";
   } else {
     navBar.classList.remove("active");
-    navBar.style.padding = "3rem 15rem 0 15rem";
     navLinks.style.transform = "trnaslateY(0)";
   }
 }
