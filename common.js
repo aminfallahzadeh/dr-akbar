a3a2c69968be9a999c55b1541ee047dadacc59b0
amin-dev-tech <amin.dev.tech@gmail.com>
2023-10-31 21:05:09 +0300

    responsive design

--- common.js
+++ common.js
@@ -17,3 +17,33 @@ function scrollFunction() {
 // handle small navbar menu
 
 const burger = document.getElementById("burger");
+const overlay = document.getElementById("overlay");
+const links = document.querySelectorAll(".overlay a");
+
+burger.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (overlay.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+});
+
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    if (!overlay.classList.contains("hidden")) {
+      overlay.classList.add("hidden");
+    } else {
+      return;
+    }
+  })
+);
+
+// handle overlay with screen size
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 900 && !overlay.classList.contains("hidden")) {
+    overlay.classList.add("hidden");
+  }
+});
